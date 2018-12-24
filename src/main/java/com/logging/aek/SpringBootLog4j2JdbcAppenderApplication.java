5555c0e6b8735f46ce16d591ac47df4e0466a48f
Anicetkeric <anicetkeric@gmail.com>
2018-12-24 17:21:07 +0000

    update pom.xlm

--- src/main/java/com/logging/aek/SpringBootLog4j2JdbcAppenderApplication.java
+++ src/main/java/com/logging/aek/SpringBootLog4j2JdbcAppenderApplication.java
@@ -1,13 +1,24 @@
 package com.logging.aek;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootLog4j2JdbcAppenderApplication {
+public class SpringBootLog4j2JdbcAppenderApplication implements CommandLineRunner {
+
+	 private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootLog4j2JdbcAppenderApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		 
+		logger.error("My first Error message");
 	}
 
 }
